99ec305579361a4717d84f8a4ed3ec357695ace8
gieonneSabijon <gieonnesabijon@gmail.com>
2023-10-13 19:38:19 -0400

    VERSION 3

--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,102 @@
 function onLoad(){
-  addLanguageListeners();
-  //window.addEventListener('scroll', handleScroll);
-  //window.addEventListener('resize', handleScroll);
-  //window.addEventListener('load', handleScroll);
+    toggleSite();
+    toggleCollapse();
+    
+    const targetDiv = document.querySelector('.Socials');
+    observer.observe(targetDiv);
 }
 
-function addLanguageListeners(){
-  const languages = document.querySelectorAll(".language_wrapper");
 
-  for(let i = 0; i < languages.length; i++){
-      languages[i].addEventListener('click', (event) => {
-          if (languages[i].classList.contains("expanded_lang")){
-              languages[i].classList.remove("expanded_lang")
-          }
-          else{
-              languages[i].classList.add("expanded_lang")
-          }
-      });
-      
-  }
-}
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // The target div is in view.
+            // You can trigger an event or execute a function here.
+            const targetDiv = document.querySelector('.Socials');
+            const titleDiv = document.querySelector('.socialsTitle');
+            titleDiv.style.animation = "slideRight 1s forwards";
+            observer.unobserve(targetDiv);
+        }
+    });
+});
 
-function isElementInViewport(element) {
-  var rect = element.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
-function handleScroll() {
-  var sections = document.querySelectorAll('.section_container');
-
-  sections.forEach(function(section) {
-    if (isElementInViewport(section)) {
-      section.classList.add('fade-in');
+function toggleSite(){
+    var page = document.querySelector('.pageContainer');
+    var powerbuttonPage = document.querySelector(".powerButtonContainer");
+    if (page){
+        if (page.style.display != "none"){
+            page.style.display = "none";
+            powerbuttonPage.style.display = "flex";
+        }
+        else {
+            encryptText();
+            page.style.display = "flex";
+            powerbuttonPage.style.display = "none";
+        }
     }
-  });
 }
 
+function toggleCollapse(){
+    var linkList = document.querySelector('.linkList');
+    var collapseIcon = document.querySelector('.collapseIcon');
+    var expandIcon = document.querySelector('.expandIcon');
+    if (linkList){
+        if (linkList.classList.contains('collapsed')){
+            linkList.classList.remove('collapsed')
+            linkList.style.display = "flex";
+            collapseIcon.style.display = "flex";
+            expandIcon.style.display = "none";
+        }
+        else {
+            linkList.classList.add('collapsed')
+            linkList.style.display = "none";
+            collapseIcon.style.display = "none";
+            expandIcon.style.display = "flex";
+            
+        }
+    }
+}
 
+function encryptText() {
+    var aboutMeParagraph = document.querySelector('.aboutMeParagraph');
+    if (aboutMeParagraph) {
+        const originalText = aboutMeParagraph.textContent;
+        const encryptedText = stringToRandomChars(originalText, originalText.length);
+        aboutMeParagraph.textContent = encryptedText;
+
+        setTimeout(function () {
+            decryptText(originalText, aboutMeParagraph, 0);
+        }); // Delay decryption for 1 second (adjust as needed)
+    }
+}
+
+function decryptText(originalText, element, index) {
+    if (index < originalText.length) {
+        const currentText = element.textContent;
+        element.textContent = setCharAt(element.textContent, index, originalText.charAt(index));
+        index++;
+        setTimeout(function () {
+            decryptText(originalText, element, index);
+        }, 10); // Delay decryption for 100 milliseconds (adjust as needed)
+    }
+}
+
+function setCharAt(str,index,chr) {
+    if(index > str.length-1) return str;
+    return str.substring(0,index) + chr + str.substring(index+1);
+}
+
+function randomChar() {
+    // Generate a random number between 32 (ASCII space) and 126 (ASCII ~)
+    const randomCharCode = Math.floor(Math.random() * (126 - 32 + 1)) + 32;
+    // Convert the random ASCII code to a character
+    return String.fromCharCode(randomCharCode);
+}
+
+function stringToRandomChars(inputString, length) {
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += randomChar();
+    }
+    return result;
+}
